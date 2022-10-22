2abcb71b3cb6db563f3cc972474ea2edb69e1aa5
Riceneeder <845541909@qq.com>
2022-10-22 17:07:42 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -52,7 +52,7 @@ module.exports = {
   // you provide. See the documentation for details.
   links: [
     {
-      name: 'user-astronaut',
+      name: 'Riceneeder',
       link: 'https://gankun.cn.ma',
     },
     {
